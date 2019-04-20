f5299534d4366decd26f30b655794b9f9c0a30bf
Alexey <iwebinet@ya.ru>
2019-04-20 23:02:29 +0300

    13.8

---
+++ 13.7/master.js
@@ -0,0 +1,44 @@
+class Enumerable {
+  // BEGIN
+  constructor(collection, operations) {
+    this.collection = collection;
+    this.operations = operations || [];
+  }
+
+  build(fn) {
+    return new Enumerable(this.collection.slice(), this.operations.concat(fn));
+  }
+
+  select(fn) {
+    return this.build(coll => coll.map(fn));
+  }
+
+  orderBy(fn, direction = 'asc') {
+    const compareResult = direction === 'asc' ? 1 : -1;
+    const comparator = (a, b) => {
+      const a1 = fn(a);
+      const b1 = fn(b);
+
+
+      if (a1 > b1) {
+        return compareResult;
+      } else if (a1 < b1) {
+        return -compareResult;
+      }
+
+      return 0;
+    };
+    return this.build(coll => coll.sort(comparator));
+  }
+
+  where(fn) {
+    return this.build(coll => coll.filter(fn));
+  }
+
+  toArray() {
+    return this.operations.reduce((acc, func) => func(acc), this.collection);
+  }
+  // END
+}
+
+export default Enumerable;
