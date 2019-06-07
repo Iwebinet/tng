30bb0b5ff08d4fb2e7c800a8906d8e3be749d294
Alexey <iwebinet@ya.ru>
2019-06-07 11:24:13 +0300

    14.6

---
+++ 14.6/master.js
@@ -0,0 +1,12 @@
+// BEGIN
+const reduce = (f, node, acc) => {
+  const newAcc = f(acc, node);
+
+  if (node.type === 'file') {
+    return newAcc;
+  }
+  return node.children.reduce((iAcc, n) => reduce(f, n, iAcc), newAcc);
+};
+
+export default reduce;
+// END
