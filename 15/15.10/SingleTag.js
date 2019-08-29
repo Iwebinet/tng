0a0297dee218382f3b4ea77121fdb223852810f5
Alexey <iwebinet@ya.ru>
2019-08-29 09:29:12 +0300

    15.10

---
+++ 15/15.10/SingleTag.js
@@ -0,0 +1,13 @@
+// BEGIN
+import Node from './Node';
+
+export default function SingleTag(name, attributes) {
+  Node.apply(this, [name, attributes]);
+}
+
+SingleTag.prototype = Object.create(Node.prototype);
+
+SingleTag.prototype.toString = function toString() {
+  return `<${this.name}${this.getAttributesAsLine()}>`;
+};
+// END
